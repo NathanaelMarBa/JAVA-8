36eb9c64769ae91e4422c7a7e08369cfaca13cfa
Nathanael <kevin_1321124060@uptecamac.edu.mx>
2025-01-08 18:17:08 -0600

    Second commit

--- src/main/java/Nathanael/Martinez/Main.java
+++ src/main/java/Nathanael/Martinez/Main.java
@@ -3,7 +3,7 @@ package Nathanael.Martinez;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hola mundo desde JAVA 8");
+        System.out.println("Hola mundo desde JAVA 8");
 
     }
 }
